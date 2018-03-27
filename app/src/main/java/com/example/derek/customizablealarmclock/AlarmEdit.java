bf746bc05243ecebf8f3d59f8fdde3d8b585d34c
dng24 <derekjng@gmail.com>
2018-03-27 11:15:05 -0400

    allAlarms page

---
+++ app/src/main/java/com/example/derek/customizablealarmclock/AlarmEdit.java
@@ -0,0 +1,13 @@
+package com.example.derek.customizablealarmclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_edit);
+    }
+}
--- app/src/main/java/com/example/derek/customizablealarmclock/ChoosePreDownloaded.java
+++ app/src/main/java/com/example/derek/customizablealarmclock/ChoosePreDownloaded.java
@@ -3,11 +3,11 @@ package com.example.derek.customizablealarmclock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class allAlarms extends AppCompatActivity {
+public class ChoosePreDownloaded extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_all_alarms);
+        setContentView(R.layout.activity_choose_pre_downloaded);
     }
 }
