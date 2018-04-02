a644c80e5e21fbc8467e8ac198236a1562104fca
dng24 <derekjng@gmail.com>
2018-04-02 10:36:40 -0400

    AllPages

---
+++ app/src/main/java/com/example/derek/customizablealarmclock/SoundName.java
@@ -0,0 +1,13 @@
+package com.example.derek.customizablealarmclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoundName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_name);
+    }
+}
