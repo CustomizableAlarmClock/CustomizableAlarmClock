3f3722e651dfb1435967455169fbfbd535cfb4fc
dng24 <derekjng@gmail.com>
2018-04-03 10:45:31 -0400

    AllPages

--- app/src/main/java/com/example/derek/customizablealarmclock/AlarmEdit.java
+++ app/src/main/java/com/example/derek/customizablealarmclock/AlarmEdit.java
@@ -9,5 +9,6 @@ public class AlarmEdit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_edit);
+        System.out.println("asdf");
     }
 }
