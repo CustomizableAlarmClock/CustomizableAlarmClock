94668d15fa6cd4916489cbf9127e2f1ef36118b7
SydneyAiyana <Sydneyh2k@yahoo.com>
2018-04-06 14:44:31 -0400

    comment
    changed .MainActivity to .Home

--- app/src/main/java/com/example/derek/customizablealarmclock/AllAlarms.java
+++ app/src/main/java/com/example/derek/customizablealarmclock/AllAlarms.java
@@ -1,7 +1,19 @@
 package com.example.derek.customizablealarmclock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class AllAlarms extends AppCompatActivity {
 
@@ -9,5 +21,42 @@ public class AllAlarms extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_alarms);
+
+        Button ButtonAddItems = findViewById(R.id.ButtonAddItems);
+        ButtonAddItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAlarmEdit();
+            }
+        });
+
     }
+
+
+    public void openAlarmEdit() {
+
+        Intent intent2  = new Intent(this, AlarmEdit.class);
+        startActivity(intent2);
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/derek/customizablealarmclock/Home.java
+++ app/src/main/java/com/example/derek/customizablealarmclock/Home.java
@@ -1,13 +1,41 @@
 package com.example.derek.customizablealarmclock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button PresstoContinueButton = findViewById(R.id.PressToContinue);
+        PresstoContinueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAllAlarms();
+            }
+        });
+
     }
+
+
+        public void openAllAlarms() {
+
+            Intent intent  = new Intent(this, AllAlarms.class);
+            startActivity(intent);
+    }
+
+
+
+
+
+
+
+
 }
